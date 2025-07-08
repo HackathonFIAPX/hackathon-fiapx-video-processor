9b26c452ab9002f4bf006b7562e38cd6f805fd0b
GregoriSimei <gregorisimei@gmail.com>
2025-07-07 22:59:48 -0300

    feat: process all events

--- src/usecase/create-video-fps/CreateVideoFpsUseCase.ts
+++ src/usecase/create-video-fps/CreateVideoFpsUseCase.ts
@@ -18,17 +18,16 @@ export class CreateVideoFpsUseCase implements ICreateVideoFpsUseCase {
     ) {}
 
     async execute(input: TCreateVideoFpsUseCaseInput): Promise<TCreateVideoFpsUseCaseOutput> {
-        const { bucket, key, eventIndex } = input;
+        const { bucket, key } = input;
         Logger.info("CreateVideoFpsUseCase", "Executing create video FPS", { input });
-        
-        if(eventIndex == 3) {
-            const videoUrl = await this.s3Handler.generatePresignedURL({ bucket, key });
-            const dirWithImages = await this.generateFPS(input, videoUrl);
-            Logger.info("CreateVideoFpsUseCase", "FPS images generated", { dirWithImages });
-            const systemFile = await this.zipper.zipFolder(dirWithImages, `user-info-test-${Date.now()}`);
 
-            await this.uploadZippedFileToS3(systemFile);
-        }
+        const videoUrl = await this.s3Handler.generatePresignedURL({ bucket, key });
+        const dirWithImages = await this.generateFPS(input, videoUrl);
+        Logger.info("CreateVideoFpsUseCase", "FPS images generated", { dirWithImages });
+        
+        const systemFile = await this.zipper.zipFolder(dirWithImages, `user-info-test-${Date.now()}`);
+
+        await this.uploadZippedFileToS3(systemFile);
     }
 
     private async generateFPS(input: TCreateVideoFpsUseCaseInput, s3VideoURL: string): Promise<string> {
