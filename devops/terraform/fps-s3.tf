c30b56b2ea38a89ae699cf6b1557bcc91610e866
GregoriSimei <gregorisimei@gmail.com>
2025-07-13 01:01:27 -0300

    fix: remove cors

--- devops/terraform/fps-s3.tf
+++ devops/terraform/fps-s3.tf
@@ -24,3 +24,15 @@ resource "aws_s3_bucket_lifecycle_configuration" "fps_expiration_rule" {
     }
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "fps_upload_cors" {
+  bucket = aws_s3_bucket.video_fps_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET", "HEAD"]
+    allowed_origins = ["*"] # "https://yoursite.com"
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
